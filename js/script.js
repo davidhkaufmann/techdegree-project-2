459f6735d84c12baa759b290c9a847f55bd1cd82
David Kaufmann <62357112+davidhkaufmann@users.noreply.github.com>
2020-06-24 11:03:22 -0400

    page update

--- js/script.js
+++ js/script.js
@@ -5,38 +5,48 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-
-//global variables
-
-const studentList = document.querySelectorAll('li');
+const listItems = document.querySelectorAll('.student-item');
+const perPage = 10;
 
 
-// But be mindful of which variables should be global and which 
-// should be locally scoped to one of the two main functions you're 
-// going to create. A good general rule of thumb is if the variable 
-// will only be used inside of a function, then it can be locally 
-// scoped to that function.
+function showPage (list, page) {
+  const startIndex = (page * perPage) - perPage;
+  const endIndex = page * perPage;
+  for (let i = 0; i < listItems.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      listItems[i].style.display = 'block';
+    } else {
+      listItems[i].style.display = 'none';
+    }
+  }
+}
 
 
+function appendPageLinks (list) {
+  const mainDiv = document.querySelector('.page')
+  const div = document.createElement('div');
+  const ul = document.createElement('ul');
+  div.className = 'pagination';
+  mainDiv.appendChild(div);
+  div.appendChild(ul);
+  for (let i = 1; i < listItems.length/10; i++) {
+    if (i > 0) {
+      const li = document.createElement('li');
+      ul.appendChild(li);
+      const a = document.createElement('a');
+      li.appendChild(a);
+      a.href = '#';
+      a.textContent = i;
+      ul.firstElementChild.firstElementChild.className = 'active';
+      for (let i = 0; i < a.length; i++) {
+        a[i].classList.remove('active');
+        console.log(a);
+      }
+    }
+  }
+}
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
+appendPageLinks();
 
 
 /*** 
